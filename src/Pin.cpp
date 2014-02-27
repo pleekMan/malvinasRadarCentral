1fd2f124a7989479fadb8e5403df1edce6524709
masOTROS <javier@masotros.com>
2014-02-27 19:45:18 -0300

    Running Rings + Navigator HotSpots TRigger

---
+++ src/Pin.cpp
@@ -0,0 +1,9 @@
+//
+//  Pin.cpp
+//  malvinasRadarCentral
+//
+//  Created by Zaira on 26/02/14.
+//
+//
+
+#include "Pin.h"
---
+++ src/Pin.h
@@ -0,0 +1,14 @@
+//
+//  Pin.h
+//  malvinasRadarCentral
+//
+//  Created by Zaira on 26/02/14.
+//
+//
+
+#ifndef __malvinasRadarCentral__Pin__
+#define __malvinasRadarCentral__Pin__
+
+#include <iostream>
+
+#endif /* defined(__malvinasRadarCentral__Pin__) */
